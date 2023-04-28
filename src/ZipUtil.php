5dd776508f5f1301b73516191aa8a6651e7783fa
wf001 <chuya.takizawa@toyokumo.co.jp>
2023-04-28 13:08:19 +0900

    Catch exceptions during file opening and extraction.

--- src/ZipUtil.php
+++ src/ZipUtil.php
@@ -6,6 +6,7 @@ use Exception;
 use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
+use RuntimeException;
 use ZipArchive;
 
 class ZipUtil
@@ -30,15 +31,28 @@ class ZipUtil
      */
     public function unzip(string $path): array
     {
-        $this->zip->open($path);
-        $dir = '/tmp/' . uniqid('zip');
-        $this->zip->extractTo($dir);
-        $files = [];
-        for ($i = 0; $i < $this->zip->numFiles; $i++) {
-            $files[] = $dir . '/' . $this->zip->getNameIndex($i);
+        if ($this->zip->open($path) !== true) {
+            throw new RuntimeException('Unknown file format');
         }
-        $this->zip->close();
-        return [$dir, $files];
+        $dir = '/tmp/' . uniqid('zip');
+        try {
+            $this->zip->extractTo($dir);
+
+            if ($this->zip->numFiles === 0) {
+                throw new RuntimeException('Extracted file is empty');
+            }
+
+            $files = [];
+            for ($i = 0; $i < $this->zip->numFiles; $i++) {
+                $files[] = $dir . '/' . $this->zip->getNameIndex($i);
+            }
+
+            return [$dir, $files];
+        } catch (Exception $e) {
+            throw new RuntimeException('Can not extract file');
+        } finally {
+            $this->zip->close();
+        }
     }
 
     /**
